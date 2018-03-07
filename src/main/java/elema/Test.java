dda30c0d14a5d29b859bb3624ec99c61b43d644b
Administrator <Administrator@TEDU-20170928ZM>
2018-03-07 21:23:22 +0800

    97测试
    Change-Id: Ie9c927e7237452bbc948da80398593ed6d804ef1
    Signed-off-by: zhangyan <214288949@qq.com>

--- src/main/java/elema/Test.java
+++ src/main/java/elema/Test.java
@@ -2,6 +2,7 @@ package elema;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("测试jabva111");
+		System.out.println("测试jabva1111111");
+		System.out.println("测试97");
 	}
 }
